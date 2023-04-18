3019e738c123db2b6730f3ceb61a8c252a9f136a
Brooks A. Jackson <brooksjacksonmanagement@gmail.com>
2023-04-18 17:43:49 -0400

    Update Rectangle.java

--- week12/Shape/Rectangle.java
+++ week12/Shape/Rectangle.java
@@ -1,15 +1,18 @@
 package week12.Shape;
 
 public class Rectangle extends Shape {
+    // Constructor for Rectangle
     public Rectangle(double height, double width) {
-        super(height, width);
+        super(height, width); // Setting height and width
     }
 
+    // Calculating the area
     public double getArea() {
-        return super.getHeight() * super.getWidth();
+        return super.getHeight() * super.getWidth(); // Formula for area
     }
 
+    // Calculating the perimeter
     public double getPerimeter() {
-        return 2 * (super.getHeight() + super.getWidth());
+        return 2 * (super.getHeight() + super.getWidth()); // Forumla for perimeter
     }
 }
