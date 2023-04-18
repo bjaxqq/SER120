932b554cd3ad6a6a3923dac0f932e8e8ee972db4
Brooks A. Jackson <brooksjacksonmanagement@gmail.com>
2023-04-18 17:43:40 -0400

    Update Triangle.java

--- week12/Shape/Triangle.java
+++ week12/Shape/Triangle.java
@@ -1,14 +1,18 @@
 package week12.Shape;
 
-public class Triangle extends Shape {
+public class Triangle extends Shape { // Inheriting from Shape
+    // Constructor for Triangle
     public Triangle(double height, double base) {
         super(height, base);
     }
 
+    // Calculating the area
     public double getArea() {
         return super.getHeight() * super.getWidth() / 2;
     }
 
+    // Calculating the perimeter
+    // Creates a hypotenuse to solve for a right triangle
     public double getPerimeter() {
         double hypotenuse = Math.sqrt(super.getHeight() * super.getHeight() + super.getWidth() * super.getWidth());
         return super.getHeight() + super.getWidth() + hypotenuse;
