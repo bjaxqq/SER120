6c9c841e1d5a6d8ad86b2b7eda24ef1bf6c90ab5
Brooks Jackson <brooksjacksonmanagement@gmail.com>
2023-04-21 17:28:34 -0400

    Create TicTacToePrinterDriver.java

---
+++ week12/Assignment3/Q2TicTacToe/TicTacToePrinterDriver.java
@@ -0,0 +1,8 @@
+package week12.Assignment3.Q2TicTacToe;
+
+public class TicTacToePrinterDriver {
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+}
