1737613bb4a2de4c3f3f0f28992fd04616829c65
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 17:52:49 -0400

    Create Card.java

---
+++ Coding Assignments/Final Project/Card.java
@@ -0,0 +1,22 @@
+public class Card {
+	
+	private final String face;
+	private final String suit;
+	
+	public Card(String cardFace, String cardSuit) {
+		this.face = cardFace;
+		this.suit = cardSuit;
+	}
+	
+	public String toString() {
+		return this.face + " of " + this.suit;
+	}
+	
+	public String getFace() {
+		return this.face;
+	}
+	
+	public String getSuit() {
+		return this.suit;
+	}
+}
