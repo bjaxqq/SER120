7427b9b44c51f87a4e60bf32253e8883c692398e
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 17:09:15 -0400

    Create Q2Vertebrate.java

---
+++ Coding Assignments/Assignment 4/Q2Vertebrate.java
@@ -0,0 +1,13 @@
+package Coding Assignments.Assignment 4;
+
+public class Q2Vertebrate {
+	private double brainSize;
+	
+	public Q2Vertebrate(double brainSize) {
+		this.brainSize = brainSize; // in cubic centimeters
+	}
+	
+	public String toString() {
+		return "brain size: " + this.brainSize;
+	}
+}
