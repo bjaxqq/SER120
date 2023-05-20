9d6e4f2237d38e434d750521d6530e97c151be1a
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 17:09:11 -0400

    Create Q2LizardDriver.java

---
+++ Coding Assignments/Assignment 4/Q2LizardDriver.java
@@ -0,0 +1,10 @@
+package Coding Assignments.Assignment 4;
+
+public class Q2LizardDriver {
+    public static void main(String[] args) {
+        // creates a new lizard object
+        Lizard lizard = new Lizard(0.9, 4.0, 10, "tropical");
+        // calls Lizard toString method
+        System.out.println(lizard);
+    }
+}
