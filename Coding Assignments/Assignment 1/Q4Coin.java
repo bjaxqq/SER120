3c738ffcbc30ad8c8693c9e367e09bb916860a6a
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 16:30:42 -0400

    Create Q4Coin.java

---
+++ Coding Assignments/Assignment 1/Q4Coin.java
@@ -0,0 +1,36 @@
+package Coding Assignments.Assignment 1;
+
+// importing random number utility
+import java.util.Random;
+
+// creating Coin class
+public class Q4Coin {
+    // creating String variable named sideUp
+    private String sideUp;
+
+    // creating default constructor for randomly determining side of coin
+    public Coin() {
+        // calling the toss function
+        toss();
+    }
+
+    // creating void member function named toss
+    public void toss() {
+        Random randNum = new Random();
+        // value for random toss of only 2 possibilities (heads or tails)
+        int res = rand.nextInt(2);
+
+        // if-statement determining what happens when either heads or tails
+        if (result == 0) {
+            sideUp = "heads";
+        }
+        else {
+            sideUp = "tails";
+        }
+    }
+
+    // creating member function named getSideUp
+    public void getSideUp() {
+        return sideUp;
+    }
+}
