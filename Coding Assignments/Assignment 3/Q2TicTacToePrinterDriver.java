5665eefb59e62608207a8c642dc99d02e321d802
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 16:46:21 -0400

    Create Q2TicTacToePrinterDriver.java

---
+++ Coding Assignments/Assignment 3/Q2TicTacToePrinterDriver.java
@@ -0,0 +1,8 @@
+package Coding Assignments.Assignment 3;
+
+public class Q2TicTacToePrinterDriver {
+    public static void main(String[] args) {
+        TicTacToe game = new TicTacToe();
+        game.play();
+    }
+}
