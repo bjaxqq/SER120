87cd8ebfeed9e34cc695a9274daa1e9be06d35f4
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-20 16:30:39 -0400

    Create Q2EmployeeTest.java

---
+++ Coding Assignments/Assignment 1/Q2EmployeeTest.java
@@ -0,0 +1,28 @@
+package Coding Assignments.Assignment 1;
+
+// creating Employee class
+public class Q2EmployeeTest {
+    // creating main function
+    public static void main(String[] args) {
+        // creating objects correlating to the 3 employees in the table
+        Employee job1 = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
+        Employee job2 = new Employee("Mark Jones", 39119, "IT", "Programmer");
+        Employee job3 = new Employee("Joy Rogers", 81774, "Manufacturing", "Engineer");
+
+        // printing the information of employee #1
+        System.out.println("Name: " + job1.getName());
+        System.out.println("ID Number: " + job1.getIdNumber());
+        System.out.println("Department: " + job1.getDepartment());
+        System.out.println("Position: " + job1.getPosition());
+
+        System.out.println("Name: " + job2.getName());
+        System.out.println("ID Number: " + job2.getIdNumber());
+        System.out.println("Department: " + job2.getDepartment());
+        System.out.println("Position: " + job2.getPosition());
+        
+        System.out.println("Name: " + job3.getName());
+        System.out.println("ID Number: " + job3.getIdNumber());
+        System.out.println("Department: " + job3.getDepartment());
+        System.out.println("Position: " + job3.getPosition());
+    }
+}
